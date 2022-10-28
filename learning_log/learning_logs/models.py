d2ccddb2147a4dcc3fa83556ed6a6056217fa25c
devdjangolse <josinelerton@gmail.com>
2022-10-28 13:10:04 +0200

    adicionada a funcionalidade de delete

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -7,13 +7,13 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     text = models.CharField(max_length=200, blank=True,null=True)
     date_added = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
 
 class Entry(models.Model):
-    topic = models.ForeignKey(Topic , on_delete=models.DO_NOTHING)
+    topic = models.ForeignKey(Topic , on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
